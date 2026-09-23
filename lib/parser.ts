25c628381da18952c89e8e15ee6d0d90427e24a2
Pariprashnaka <pariprashnaka@gmail.com>
2026-09-23 12:40:32 +0530

    fix: officeparser method name

--- lib/parser.ts
+++ lib/parser.ts
@@ -29,7 +29,7 @@ export async function parseFile(buffer: Buffer, mimeType: string, fileName: stri
   if (ext === 'pptx' || mimeType.includes('presentationml')) {
     const { default: officeParser } = await import('officeparser')
     return new Promise((resolve, reject) => {
-      officeParser.parseOfficeAsync(buffer, (data: string, err: Error) => {
+      officeParser.parseOffice(buffer, (data: string, err: Error) => {
         if (err) reject(err)
         else resolve(data)
       })
